0cf6275b69a9941bafaa95167aa12714b4c48c99
Matteo Coppola <mcoppola94@hotmail.it>
2020-05-27 15:48:27 +0200

    Fixed duplicated questions and different with same question

--- masters/data-and-text-mining/markdown-quiz/scrape.py
+++ masters/data-and-text-mining/markdown-quiz/scrape.py
@@ -21,18 +21,19 @@ for r, d, f in os.walk(path):
 
 print(len(files), "quiz reviews to scrape")
 
-answeredQuestions = []
-unansweredQuestions = []
+answeredQuestionsHash = []
+unansweredQuestionsHash = []
 
 for f in files:
 	print("Processing", f)
 
-	with open(f, encoding='utf8') as fp:
+	with open(f, encoding='utf-8') as fp:
 		soup = BeautifulSoup(fp, features="html.parser")
 
 
 	data = []
 	title = soup.title.text[6:].replace(':', '')
+	title = title.replace(' Attempt review', '')
 	questions = soup.find_all(class_="que")
 
 	try:
@@ -43,32 +44,44 @@ for f in files:
 
 	for question in questions:
 		obj = {}
-		obj["chapter"]= title
-		if (question.find_all(class_="grade")[0].text == "Mark 1.00 out of 1.00"):
-			obj["question"] = question.find_all(class_="qtext")[0].text.strip().replace("\t", "").replace("\n"," ")
-			if(not(DoesListContainsQuestion(obj["question"], answeredQuestions))):
-				questionSlug = hashlib.sha224(obj["question"].encode('utf-8')).hexdigest()
-				print("Found full grade question", questionSlug[-8:])
-				obj["answers"] = []
+		obj["chapter"] = title
+
+		if question.find_all(class_="grade")[0].text == "Mark 1.00 out of 1.00":
+			obj["question"] = question.find_all(class_="qtext")[0].text.strip().replace("\t", "").replace("\n", " ").replace(u"\u03c6", "PHI")
+#				questionSlug = hashlib.sha224(obj["question"].encode('utf-8')).hexdigest()
+#				print("Found full grade question", questionSlug[-8:])
+			obj["answers"] = []
+			answerObj = {}
+			corrects=0
+			answersText = []  # list of all possible answers for later use
+			for answer in question.find_all(class_=["r0", "r1"]):
 				answerObj = {}
-				corrects=0
-				for answer in question.find_all(class_=["r0", "r1"]):
+				answerObj["text"] = answer.text.replace(u"\u03c6", "PHI")
+				answersText.append(answerObj["text"])
 
-					answerObj = {}
-					answerObj["text"] = answer.text
-					if "checked" in answer.input.attrs:
-						answerObj["correct"] = True
-						corrects+=1
-					else:
-						answerObj["correct"] = False
+				if "checked" in answer.input.attrs:
+					answerObj["correct"] = True
+					corrects+=1
+				else:
+					answerObj["correct"] = False
 
-					obj["answers"].append(answerObj)
-				print(" with", len(obj["answers"]), "options,", corrects, "correct")
+				obj["answers"].append(answerObj)
+#				print(" with", len(obj["answers"]), "options,", corrects, "correct")
 
-				obj["meta"] = {}
-				obj["meta"]["schemaver"] = "2"
-				obj["meta"]["hash"] = str(questionSlug)[-8:]
+			# Get hash of question and save it for later comparison
+			answersText.sort()
+			textToHash = obj["question"]
+			for text in answersText:
+				textToHash += text
+			textToHash = textToHash.replace(" ", "").replace("\n", "").replace("\t", "")
+			questionSlug = hashlib.sha224(textToHash.encode('utf-8')).hexdigest()
+			print("TextToHash for this question: \"", textToHash, "\" ", questionSlug)
 
+			obj["meta"] = {}
+			obj["meta"]["schemaver"] = "2"
+			obj["meta"]["hash"] = str(questionSlug)[-8:]
+
+			if(not(DoesListContainsQuestion(questionSlug, answeredQuestionsHash))):
 				try:
 					os.mkdir("dumps/"+title)
 					print("Directory " , title ,  " Created ")
@@ -77,47 +90,60 @@ for f in files:
 				with open("dumps/"+title+"/"+str(questionSlug)[-8:]+'.json', 'w') as outfile:
 					json.dump(obj, outfile)
 
-				answeredQuestions.append(obj["question"])
+				answeredQuestionsHash.append(questionSlug)
 
 for f in files:
 	print("Processing unanswered questions", f)
 
-	with open(f, encoding='utf8') as fp:
+	with open(f, encoding='utf-8') as fp:
 		soup = BeautifulSoup(fp, features="html.parser")
 
 	data = []
 	title = "Unanswered " + soup.title.text[6:].replace(':', '')
+	title = title.replace(' Attempt review', '')
 	questions = soup.find_all(class_="que")
 
 	for question in questions:
 		obj = {}
 		obj["chapter"]= title
 
-		if(question.find_all(class_="grade")[0].text != "Mark 1.00 out of 1.00"):
-			obj["question"] = question.find_all(class_="qtext")[0].text.strip().replace("\t", "").replace("\n", " ")
-			if (not(DoesListContainsQuestion(obj["question"], answeredQuestions)) and not(DoesListContainsQuestion(obj["question"], unansweredQuestions))):
-				questionSlug = hashlib.sha224(obj["question"].encode('utf-8')).hexdigest()
-				print("Found wrong answered question", questionSlug[-8:])
-				obj["answers"] = []
+		if question.find_all(class_="grade")[0].text != "Mark 1.00 out of 1.00":
+			obj["question"] = question.find_all(class_="qtext")[0].text.strip().replace("\t", "").replace("\n", " ").replace(u"\u03c6", "PHI")
+			#				questionSlug = hashlib.sha224(obj["question"].encode('utf-8')).hexdigest()
+			#				print("Found full grade question", questionSlug[-8:])
+			obj["answers"] = []
+			answerObj = {}
+			answersText = []  # list of all possible answers for later use
+			for answer in question.find_all(class_=["r0", "r1"]):
 				answerObj = {}
+				answerObj["text"] = answer.text.replace(u"\u03c6", "PHI")
+				answersText.append(answerObj["text"])
 
-				for answer in question.find_all(class_=["r0", "r1"]):
-					answerObj = {}
-					answerObj["text"] = answer.text
-					answerObj["correct"] = False
+				answerObj["correct"] = False
 
-					obj["answers"].append(answerObj)
+				obj["answers"].append(answerObj)
+			#				print(" with", len(obj["answers"]), "options,", corrects, "correct")
 
-				obj["meta"] = {}
-				obj["meta"]["schemaver"] = "2"
-				obj["meta"]["hash"] = str(questionSlug)[-8:]
+			# Get hash of question and save it for later comparison
+			answersText.sort()
+			textToHash = obj["question"]
+			for text in answersText:
+				textToHash += text
+			textToHash = textToHash.replace(" ", "").replace("\n", "").replace("\t", "")
+			questionSlug = hashlib.sha224(textToHash.encode('utf-8')).hexdigest()
+			print("TextToHash for this question: \"", textToHash, "\" ", questionSlug)
 
+			obj["meta"] = {}
+			obj["meta"]["schemaver"] = "2"
+			obj["meta"]["hash"] = str(questionSlug)[-8:]
+
+			if not (DoesListContainsQuestion(questionSlug, answeredQuestionsHash)) and not (DoesListContainsQuestion(questionSlug, unansweredQuestionsHash)):
 				try:
-					os.mkdir("dumps/"+title)
-					print("Directory " , title ,  " Created ")
+					os.mkdir("dumps/" + title)
+					print("Directory ", title, " Created ")
 				except FileExistsError:
 					pass
-				with open("dumps/"+title+"/"+str(questionSlug)[-8:]+'.json', 'w') as outfile:
+				with open("dumps/" + title + "/" + str(questionSlug)[-8:] + '.json', 'w') as outfile:
 					json.dump(obj, outfile)
 
-				unansweredQuestions.append(obj["question"])
+				unansweredQuestionsHash.append(questionSlug)
